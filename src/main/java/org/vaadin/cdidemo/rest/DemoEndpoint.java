552139ef74b64d6a57e389b54ecd55feff561954
tatulund <tatu@vaadin.com>
2022-08-10 11:31:00 +0300

    Sanitize input from REST

--- src/main/java/org/vaadin/cdidemo/rest/DemoEndpoint.java
+++ src/main/java/org/vaadin/cdidemo/rest/DemoEndpoint.java
@@ -9,6 +9,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.slf4j.Logger;
 import org.vaadin.cdidemo.eventbus.EventBus;
 
@@ -26,6 +28,7 @@ public class DemoEndpoint {
     @Path("/hello/{parameter}")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@PathParam("parameter") String parameter) {
+        parameter = Jsoup.clean(parameter, Safelist.none());
         logger.info("REST GET hello: "+parameter);
         RestMessageEvent event = new RestMessageEvent(parameter);
         eventBus.post(event);
